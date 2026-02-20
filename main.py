c9868959f026913622df8d95b2af2b6401524c01
Federico <gurrea.fd@gmail.com>
2026-02-20 16:30:35 -0300

    configuración de acumular de tiempo

--- main.py
+++ main.py
@@ -1,17 +1,18 @@
 import time
 import cv2
 import numpy as np
+import pandas as pd
 from time_acumulador import TimeAcumulador
 from detector import detect_pose, detect_cell_phone, is_using_phone
 
 acc = TimeAcumulador()
-cap = cv2.VideoCapture(0)
+cap = cv2.VideoCapture("video 3.mp4")
 Persona_id = 1
 if not cap.isOpened():
     print("Error: No se pudo abrir camara")
     exit()
 
-
+buffer_estatus = []
 while True:
     ret, frame = cap.read()
 
@@ -21,8 +22,10 @@ while True:
     keypoints = detect_pose(frame)
     phones = detect_cell_phone(frame)
     using = is_using_phone(frame, keypoints, phones)
+    instant_using = using(window=10)
+    stable_using = instant_using.mean()
 
-    acc.update(Persona_id, using)
+    acc.update(Persona_id, stable_using)
 
     cv2.imshow("Frame", frame)
 
