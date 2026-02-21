20b0535bc7fa6306a06ce684bb8de53c0488485a
Federico <gurrea.fd@gmail.com>
2026-02-21 20:45:21 -0300

    Mejora metrica de tiempo

--- main.py
+++ main.py
@@ -6,13 +6,15 @@ from time_acumulador import TimeAcumulador
 from detector import detect_pose, detect_cell_phone, is_using_phone
 
 acc = TimeAcumulador()
-cap = cv2.VideoCapture("video 3.mp4")
+cap = cv2.VideoCapture("video 4.mp4")
 Persona_id = 1
 if not cap.isOpened():
     print("Error: No se pudo abrir camara")
     exit()
 
 buffer_estatus = []
+stable_using = False
+fps = cap.get(cv2.CAP_PROP_FPS)
 while True:
     ret, frame = cap.read()
 
@@ -22,18 +24,28 @@ while True:
     keypoints = detect_pose(frame)
     phones = detect_cell_phone(frame)
     using = is_using_phone(frame, keypoints, phones)
-    instant_using = using(window=10)
-    stable_using = instant_using.mean()
+    buffer_estatus.append(using)
+    if len(buffer_estatus) > 10:
+        buffer_estatus.pop(0)
+    true_count = buffer_estatus.count(True)
 
-    acc.update(Persona_id, stable_using)
+    if true_count > len(buffer_estatus) / 2:
+        stable_using = True
+    elif true_count < len(buffer_estatus) / 2:
+        stable_using = False
+    # Precisión de tiempo    
+    frame_number = cap.get(cv2.CAP_PROP_POS_FRAMES)
+    timestamp = frame_number / fps
+    acc.update(Persona_id, stable_using, timestamp)
 
     cv2.imshow("Frame", frame)
 
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
-
+# Mejora de reporte de tiempo    
+final_timestamp = cap.get(cv2.CAP_PROP_POS_MSEC) / 1000.0
+acc.close_all(final_timestamp)
 cap.release()
 cv2.destroyAllWindows()
 
 print("Persona 1:", acc.get_total_time(1))
-
